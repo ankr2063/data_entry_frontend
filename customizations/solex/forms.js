d892d46dc345f89e12d13c768d2b5691c50001e3
Harsh Jain <jainharsh3003@gmail.com>
2025-12-26 16:25:48 +0530

    Cusom scripts bug fix

--- customizations/solex/forms.js
+++ customizations/solex/forms.js
@@ -3,10 +3,10 @@ state.listenForAction('FORM_FIELD_BLUR').subscribe((data) => {
     const fieldValue = $(`[data-form-element="${data.formFieldIdx}"`).val();
 
     if (!fieldValue && !data.config.optional) {
-        console.log('value not provided');
+        u.toast.showError({ msg: 'value not provided', direction: 'topRight' });
     } else if (fieldValue < data.config.min_value_allowed) {
-        console.log('value out of range (min)');
+        u.toast.showError({ msg: 'value out of range (min)', direction: 'topRight' });
     } else if (fieldValue > data.config.max_value_allowed) {
-        console.log('value out of range (max)');
+        u.toast.showError({ msg: 'value out of range (max)', direction: 'topRight' });
     }
 });
